a952796e194c38f2f0515045e29fc68e0666d759
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2024-06-03 17:03:42 +0900

    ✨ slide 추가

--- src/js/main.js
+++ src/js/main.js
@@ -16,35 +16,62 @@ document.addEventListener("DOMContentLoaded", function (event) {
     //         markers: true,
     //         scrub: true
     //     }
-    // }) 
+    // })
 });
 
 $(function () {
     // Initialize Swiper
-    var mainSwiper = new Swiper(".main-swiper", {
-        autoplay: { delay: 4000, disableOnInteraction: false },
-        speed: 800,
-        loop: true,
-        effect: "creative",
-        creativeEffect: { prev: { translate: [0, 0, -400] }, next: { translate: ["100%", 0, 0] } },
-        navigation: { nextEl: ".main-slide .swiper-button-next", prevEl: ".main-slide .swiper-button-prev" },
-        pagination: {
-            el: ".main-slide .swiper-pagination",
-            type: "fraction",
-            clickable: true,
-            formatFractionCurrent: function (number) {
-                return ("0" + number).slice(-2);
+    // var mainSwiper = new Swiper(".main-swiper", {
+    //     autoplay: { delay: 4000, disableOnInteraction: false },
+    //     speed: 800,
+    //     loop: true,
+    //     effect: "creative",
+    //     creativeEffect: { prev: { translate: [0, 0, -400] }, next: { translate: ["100%", 0, 0] } },
+    //     navigation: { nextEl: ".main-slide .swiper-button-next", prevEl: ".main-slide .swiper-button-prev" },
+    //     pagination: {
+    //         el: ".main-slide .swiper-pagination",
+    //         type: "fraction",
+    //         clickable: true,
+    //         formatFractionCurrent: function (number) {
+    //             return ("0" + number).slice(-2);
+    //         },
+    //         formatFractionTotal: function (number) {
+    //             return ("0" + number).slice(-2);
+    //         },
+    //     },
+    // });
+
+    // // Sync text box with Swiper slide
+    // mainSwiper.on("slideChange", function () {
+    //     var currentIndex = mainSwiper.realIndex + 1;
+    //     $(".text-box").removeClass("on");
+    //     $(".text-box[data-slide='" + currentIndex + "']").addClass("on");
+    // });
+
+    var mySwiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        // pagination: {
+        //     el: ".main-slide .swiper-pagination",
+        //     clickable: true,
+        // },
+        navigation: {
+            nextEl: ".circle-button.swiper-button-next",
+            prevEl: ".circle-button.swiper-button-prev",
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 20,
             },
-            formatFractionTotal: function (number) {
-                return ("0" + number).slice(-2);
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 10,
             },
         },
-    });
-
-    // Sync text box with Swiper slide
-    mainSwiper.on("slideChange", function () {
-        var currentIndex = mainSwiper.realIndex + 1;
-        $(".text-box").removeClass("on");
-        $(".text-box[data-slide='" + currentIndex + "']").addClass("on");
     });
 });
